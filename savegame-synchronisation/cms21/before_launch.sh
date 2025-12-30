6dba82fa56b347c628ab24bf30ed6ba85b640fc9
Konrad Guzek <konrad@guzek.uk>
2025-12-30 02:31:16 +0100

    remove savegame sync scripts

--- savegame-synchronisation/cms21/before_launch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-SAVEGAME_DIR="/home/konrad/Games/Heroic/Prefixes/default/Car Mechanic Simulator 2021/drive_c/users/konrad/AppData/LocalLow/Red Dot Games/Car Mechanic Simulator 2021/"
-cd "$SAVEGAME_DIR"
-git pull --force
-
